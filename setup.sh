14a897d75f0d7e0ac608720e5cb2c90454f2dd8a
4ce <88108711+4cecoder@users.noreply.github.com>
2022-05-07 20:22:18 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 
 apt update && apt upgrade - y;
-apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt -y install git apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list;
 apt update;
