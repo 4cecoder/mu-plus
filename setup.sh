7d8ce74b855ecd6b162de2e5311e9703013739fa
4ce <88108711+4cecoder@users.noreply.github.com>
2022-05-07 20:22:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list;
 apt update;
 apt install -y docker-ce docker-ce-cli containerd.io;
+cd ~/;
 git clone https://github.com/MUnique/OpenMU;
-cd OpenMU;
+cd ~/OpenMU;
